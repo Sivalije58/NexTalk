baaecccad729927c5a2013a1117e136be09cf172
Vasilije Arsenović <arsenovicvasilije75@gmail.com>
2025-12-29 23:54:51 +0100

    Login.jsx updated.

--- web-chat/frontend/src/Login.jsx
+++ web-chat/frontend/src/Login.jsx
@@ -9,7 +9,7 @@ function Login({ setUsername }) {
 
     try {
       // 🟢 Send a POST request to the backend to add/return a user.
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://nextalk-backend-v4df.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username: name.trim() }),
